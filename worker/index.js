977eea9db3b56b859d11209366cdca31f8756c48
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2020-11-16 13:35:52 -0700

    cron job update to 08:00 every day

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,5 @@
 var CronJob = require('cron').CronJob;
 const fetchProperties = require('./tasks/fetch-pa-api');
-// Fetch edmonton properties
-var job = new CronJob('* * * * *', fetchProperties, null, true, 'America/Los_Angeles');
+// Fetch edmonton properties every at 08:00
+var job = new CronJob('0 8 * * *', fetchProperties, null, true, 'America/Los_Angeles');
 job.start();
